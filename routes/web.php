ccb2e38870a6ecde69a27c0f15a0e367eec0ab55
fpestunov <fpestunov@gmail.com>
2019-03-03 12:11:36 +0400

    fix: add Laravel Cache driver

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Str;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-function remember($key, $minutes, $callback) {
-    if ($value = Redis::get($key)) {
-        return json_decode($value);
-    }
-
-    Redis::setex($key, $minutes, json_encode($value = $callback()));
-
-    return $value;
-
-}
-
 Route::get('/', function () {
-
-    return remember('articles.all', 60 * 60, function () {
+    return Cache::remember('articles.all', 60 * 60, function () {
+        // dd('test redis works...');
         return App\Article::all();
     });
 });
